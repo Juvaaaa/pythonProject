dc0d5efb978ac0f3336bc5fbc84abf4a8b78b527
Juvaaaa <juvaaaa@mail.ru>
2022-08-18 21:51:20 +0300

    homework

---
+++ tip.py
@@ -0,0 +1,18 @@
+import math
+print(1**100)
+print(2**16)
+print(2-1000)
+print(2+2*2)
+print(60/3)
+print(math.sqrt(144))
+
+x1=(2+5*14)
+print(x1)
+
+x2=66-11
+print(x2)
+print(66-x2)
+
+x=(16-8)**2
+print(math.sqrt(x))
+print(math.sqrt(x)+8)
